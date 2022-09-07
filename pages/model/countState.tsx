5294f5b5d9f0d4e09cf50dd70beacf553ea7faee
matsumo-and <yh134.toisanda@gmail.com>
2022-09-07 22:56:19 +0900

    create counter with recoil

--- pages/model/countState.tsx
+++ pages/model/countState.tsx
@@ -1,15 +1,15 @@
 import { atom, selector } from 'recoil'
 
-export const hogeState = atom<number>({
+export const countAtom = atom<number>({
   key: 'count',
   default: 60,
 })
 
-export const hogeLengthSelector = selector<number>({
+export const countSelector = selector<number>({
   key: 'countSelector',
   get: ({ get }) => {
     // get()で任意のatomの値を取得
-    const count: number = get(hogeState)
+    const count: number = get(countAtom)
     return count || 60
   },
 })
